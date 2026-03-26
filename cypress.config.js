b707895c99bba3c22a4b5101718c27596effbbe1
trolliajn <trolliajn@gmail.com>
2026-03-26 21:46:04 +0100

    added reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,19 @@ import { defineConfig } from 'cypress';
 module.exports = defineConfig({
   allowCypressEnv: false,
 
+  //reporter: 'cypress-multi-reporters',
+  reporter: 'cypress-multi-reporters',
+
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+
   e2e: {
     baseUrl: 'https://practicesoftwaretesting.com',
     specPattern: 'cypress/e2e/tests/*.cy.js',
-    supportFile: false
+    supportFile: false,
   },
 });
